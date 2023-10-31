b6349993776fbf751ae3713534a73fe8eb835cc9
RegisLewis <regisaslewis@gmail.com>
2023-10-30 18:43:36 -0700

    create and retrieve with Flask-Restful

--- server/app.py
+++ server/app.py
@@ -17,7 +17,47 @@ db.init_app(app)
 api = Api(app)
 
 class Home(Resource):
-    pass
+    def get(self):
+        response_dict = {
+            "message": "Welcome to the Newsletter RESTful API",
+        }
+
+        response = make_response(response_dict, 200)
+
+        return response
+api.add_resource(Home, "/")
+
+class Newsletters(Resource):
+    def get(self):
+        response_dict_list = [n.to_dict() for n in Newsletter.query.all()]
+        return make_response(response_dict_list, 200)
+    
+    def post(self):
+        new_record = Newsletter(
+            title=request.form["title"],
+            body=request.form["body"],
+        )
+
+        db.session.add(new_record)
+        db.session.commit()
+
+        return make_response(new_record.to_dict(), 201)
+api.add_resource(Newsletters, "/newsletters")
+
+class NewsletterByID(Resource):
+    def get(self, id):
+
+        response = Newsletter.query.filter_by(id = id).first()
+
+        if response:
+            body = response.to_dict()
+            status = 200
+        else:
+            body = {"message": f"ID# {id} not found"}
+            status = 404
+
+        return make_response(body, status)
+api.add_resource(NewsletterByID, "/newsletters/<int:id>")
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
